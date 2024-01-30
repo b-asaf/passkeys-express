a1bab90ed471b366d4693ec448ba7b22e6623e78
asaf bar <assafbar81@gmail.com>
2024-01-30 10:41:20 +0200

    1. move routes from index.js to routes folder 2. connect business logic inside a controller with relevant route

---
+++ app/controllers/pages.js
@@ -0,0 +1,11 @@
+class PagesController {
+  welcome(req, res, next) {
+    if (!req.user) {
+      return res.send("Hello Universe");
+    }
+
+    next();
+  }
+}
+
+module.exports = PagesController;
